e196cb8edffd18d00912283161af0278f99e10b3
katsuwo <ubuntu@ip-172-31-2-25.ap-northeast-1.compute.internal>
2021-07-31 05:59:19 +0000

    follow, unfollow

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
+#アプリケーションコントローラーを継承してるクラス。
 class MicropostsController < ApplicationController
+  #アクションを行う前にログインが必須になる。
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
